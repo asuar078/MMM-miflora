2e656157bce3f508f9b7162409838060dd754b1d
Arian Suarez <arian.suarez001@gmail.com>
2021-03-14 12:34:39 -0400

    Added a comment for all parameters

--- MMM-miflora.js
+++ MMM-miflora.js
@@ -3,16 +3,19 @@ Module.register("MMM-miflora", {
     defaults: {
         updateInterval: 60, // Seconds
         titleText: "Plant Life",
-        units: "metric",
+        // units: "metric",
+        units: "imperial",
         tableClass: "small",
         decimalSymbol: ".",
+        maxMsgTimeDiff_min: 30,
+        nameCharLimit: 30
     },
 
     start: function () {
         Log.info("Starting module: " + this.name);
 
         this.SoilMoistureLimit = {
-            min: 22,
+            min: 20,
             max: 70,
             unit: '%'
         }
@@ -41,7 +44,7 @@ Module.register("MMM-miflora", {
                 address: 'c4:7c:8d:6b:ca:9e',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 100, firmware: '3.2.4'},
-                sensorValues: {temperature: 22.4, lux: 230, moisture: 0, fertility: 0},
+                sensorValues: {temperature: 5.0, lux: 500, moisture: 5, fertility: 0},
                 friendlyName: 'lavender',
                 timeStamp: 1615157554387
             },
@@ -49,7 +52,7 @@ Module.register("MMM-miflora", {
                 address: 'c4:7c:8d:6b:cc:be',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 40, firmware: '3.2.4'},
-                sensorValues: {temperature: 9.4, lux: 30000, moisture: 50, fertility: 0},
+                sensorValues: {temperature: 10.5, lux: 2000, moisture: 15, fertility: 0},
                 friendlyName: 'Potato',
                 timeStamp: Date.now()
             },
@@ -57,7 +60,7 @@ Module.register("MMM-miflora", {
                 address: 'c4:7c:8d:3c:cb:73',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 95, firmware: '3.2.4'},
-                sensorValues: {temperature: 27.4, lux: 700, moisture: 20, fertility: 10},
+                sensorValues: {temperature: 20.9, lux: 5000, moisture: 30, fertility: 10},
                 friendlyName: 'c4:7c:8d:3c:cb:73',
                 timeStamp: Date.now()
             },
@@ -65,7 +68,7 @@ Module.register("MMM-miflora", {
                 address: 'c4:8c:ad:6b:ca:9e',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 100, firmware: '3.2.4'},
-                sensorValues: {temperature: 22.4, lux: 12000, moisture: 25, fertility: 0},
+                sensorValues: {temperature: 32.4, lux: 12000, moisture: 45, fertility: 0},
                 friendlyName: 'Snow ball',
                 timeStamp: 1615157554387
             },
@@ -73,7 +76,7 @@ Module.register("MMM-miflora", {
                 address: 'c4:7c:8d:6b:cc:be',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 70, firmware: '3.2.4'},
-                sensorValues: {temperature: 9.4, lux: 70000, moisture: 59, fertility: 0},
+                sensorValues: {temperature: 45.4, lux: 70000, moisture: 60, fertility: 0},
                 friendlyName: 'Gary',
                 timeStamp: Date.now()
             },
@@ -81,7 +84,7 @@ Module.register("MMM-miflora", {
                 address: 'c4:7c:8d:3c:cb:76',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 95, firmware: '3.2.4'},
-                sensorValues: {temperature: 27.4, lux: 8000, moisture: 20, fertility: 10},
+                sensorValues: {temperature: 27.4, lux: 8000, moisture: 70, fertility: 10},
                 friendlyName: 'c4:7c:8d:3c:cb:76',
                 timeStamp: Date.now()
             },
@@ -89,24 +92,16 @@ Module.register("MMM-miflora", {
                 address: 'c4:8c:ad:6b:ca:9e',
                 type: 'MiFloraMonitor',
                 firmwareInfo: {battery: 100, firmware: '3.2.4'},
-                sensorValues: {temperature: 22.4, lux: 12000, moisture: 25, fertility: 0},
+                sensorValues: {temperature: 22.4, lux: 82000, moisture: 80, fertility: 0},
                 friendlyName: 'Snow ball II',
                 timeStamp: 1615157554387
             },
             {
                 address: 'c4:7c:8d:6b:cc:be',
                 type: 'MiFloraMonitor',
-                firmwareInfo: {battery: 70, firmware: '3.2.4'},
-                sensorValues: {temperature: 9.4, lux: 70000, moisture: 59, fertility: 0},
+                firmwareInfo: {battery: 4, firmware: '3.2.4'},
+                sensorValues: {temperature: 14.1, lux: 70000, moisture: 86, fertility: 0},
                 friendlyName: 'Patrick',
-                timeStamp: Date.now()
-            },
-            {
-                address: 'c4:7c:8d:3c:cb:76',
-                type: 'MiFloraMonitor',
-                firmwareInfo: {battery: 95, firmware: '3.2.4'},
-                sensorValues: {temperature: 42.4, lux: 8000, moisture: 80, fertility: 10},
-                friendlyName: 'c4:7c:8d:3c:cb:76',
                 timeStamp: Date.now()
             },
         ]
@@ -296,35 +291,93 @@ Module.register("MMM-miflora", {
         this.addUnitRow(tableHeader)
     },
 
+    getTempComment: function (temp_c) {
+
+        if (temp_c < this.TemperatureLimit.min) {
+            return "too cold"
+        }
+
+        if (temp_c > this.TemperatureLimit.max) {
+            return "too hot"
+        }
+
+        return ""
+    },
+
+    getBatteryComment: function (battery_level) {
+
+        if (battery_level < this.BatteryLimit.min) {
+            return "low battery"
+        }
+
+        return ""
+    },
+
     getLightIntensityComment: function (lux) {
-        if (lux < 500) {
+
+        if (lux <= 500) {
             return "no light"
         }
 
         // Low light plants
         // Lux 500-2500
-        if (lux > 500 && lux < 2500) {
+        if (lux <= 2500) {
             return "low light"
         }
 
         // Medium light plants
         // Lux 2500-10000
-        if (lux > 2500 && lux < 10000) {
+        if (lux <= 10000) {
             return "medium light"
         }
 
         // Bright light plants
         // Lux 10000-20000
-        if (lux > 10000 && lux < 20000) {
+        if (lux <= 20000) {
             return "indirect light"
         }
 
         // Very bright light plants
         // Lux 20000-50000
-        if (lux > 20000) {
-            return "direct light"
+        return "direct light"
+    },
+
+    getComComment: function (comTimeDiff_min) {
+
+        if (comTimeDiff_min > this.config.maxMsgTimeDiff_min) {
+            return "lost sensor"
         }
 
+        return ""
+    },
+
+    getMoistureComment: function (moisture) {
+
+        if (moisture <= 19) {
+            return "very dry"
+        }
+
+        if (moisture <= 30) {
+            return "tolerant dry"
+        }
+
+        if (moisture <= 45) {
+            return "lightly moist"
+        }
+
+        if (moisture <= 60) {
+            return "moist"
+        }
+
+        if (moisture <= 75) {
+            return "lightly wet"
+        }
+
+        if (moisture <= 85) {
+            return "wet"
+        }
+
+        return "very wet"
     },
 
     addValueCommentRow: function (tableBody, sensors) {
@@ -334,7 +387,7 @@ Module.register("MMM-miflora", {
         tableBody.appendChild(commentRow);
 
         let tempCommentCell = document.createElement("td");
-        tempCommentCell.innerHTML = `too hot`
+        tempCommentCell.innerHTML = this.getTempComment(sensors.sensorValues.temperature)
         tempCommentCell.className = "flora-comment-cell flora-inner-cell-padding";
         // tempCommentCell.rowSpan = 2
         commentRow.appendChild(tempCommentCell);
@@ -346,19 +399,20 @@ Module.register("MMM-miflora", {
         commentRow.appendChild(lightCommentCell);
 
         let moistureCommentCell = document.createElement("td");
-        moistureCommentCell.innerHTML = `tolerant dry`
+        moistureCommentCell.innerHTML = this.getMoistureComment(sensors.sensorValues.moisture)
         moistureCommentCell.className = "flora-comment-cell flora-inner-cell-padding";
         // tempCommentCell.rowSpan = 2
         commentRow.appendChild(moistureCommentCell);
 
         let batteryCommentCell = document.createElement("td");
-        batteryCommentCell.innerHTML = `low battery`
+        batteryCommentCell.innerHTML = this.getBatteryComment(sensors.firmwareInfo.battery)
         batteryCommentCell.className = "flora-comment-cell flora-inner-cell-padding";
         // tempCommentCell.rowSpan = 2
         commentRow.appendChild(batteryCommentCell);
 
         let comCommentCell = document.createElement("td");
-        comCommentCell.innerHTML = `lost sensor`
+        let timeDiff = this.getTimeDiffMin(sensors.timeStamp)
+        comCommentCell.innerHTML = this.getComComment(timeDiff)
         comCommentCell.className = "flora-comment-cell flora-outer-cell-padding";
         // tempCommentCell.rowSpan = 2
         commentRow.appendChild(comCommentCell);
@@ -389,11 +443,11 @@ Module.register("MMM-miflora", {
         }
 
         let tempCell = document.createElement("td");
-        let tempVal = sensors.sensorValues.temperature.toString()
-        if (this.config.unit === "imperial") {
+        let tempVal = sensors.sensorValues.temperature
+        if (this.config.units === "imperial") {
             tempVal = this.celsiusToFahren(tempVal)
         }
-        tempCell.innerHTML = tempVal.replace(".", this.config.decimalSymbol) + degreeLabel;
+        tempCell.innerHTML = tempVal.toFixed(1).replace(".", this.config.decimalSymbol) + degreeLabel;
         if (sensors.sensorValues.temperature >= this.TemperatureLimit.min
             && sensors.sensorValues.temperature <= this.TemperatureLimit.max) {
             tempCell.className = "flora-cell flora-good-data flora-inner-cell-padding";
@@ -444,8 +498,8 @@ Module.register("MMM-miflora", {
 
         let timeDiff = this.getTimeDiffMin(sensors.timeStamp)
         Log.info(`data time diff: ${timeDiff}`)
-        if (timeDiff > 30) {
-            timeCell.innerHTML = "> 30 min"
+        if (timeDiff > this.config.maxMsgTimeDiff_min) {
+            timeCell.innerHTML = `> ${this.config.maxMsgTimeDiff_min} min`
             timeCell.className = "flora-cell flora-bad-data flora-outer-cell-padding";
         } else {
             timeCell.innerHTML = `${timeDiff} min ago`
